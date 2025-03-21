1affe4744a45fbc8dede19f5ae88df7389804688
Awshae <123233077+Awshae@users.noreply.github.com>
2025-03-21 15:03:00 +0500

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+def setup_logger(log_file):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+logger = setup_logger(config.LOG_FILE)
